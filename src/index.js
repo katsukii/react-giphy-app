8f63c390278e5654a0c7a471354d8779d89afb4b
Yusuke Katsuki <katsuki.yusuke@gmail.com>
2019-04-29 17:23:50 +0900

    img要素をstateをもとにレンダリングする

--- src/index.js
+++ src/index.js
@@ -1,16 +1,45 @@
 import React from "react";
 import { render } from "react-dom";
+import axios from "axios";
 
-import "./styles.css";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { gifUrlList: [] };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  componentDidMount() {
+    this.giphyApi();
+  }
+
+  renderImageList(list) {
+    const imageList = list.map(url => {
+      return (
+        <li>
+          <img src={url} />
+        </li>
+      );
+    });
+    return <ul>{imageList}</ul>;
+  }
+
+  render() {
+    return <div>{this.renderImageList(this.state.gifUrlList)}</div>;
+  }
+  giphyApi() {
+    const search = "cat";
+    const key = "xyfK2JliY3njRYtDVlL7mnqSWM4Kb8Ap";
+    const limit = 10;
+
+    const url = `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${key}&limit=${limit}`;
+
+    axios.get(url).then(res => {
+      const data = res.data.data;
+      const imageUrlList = data.map(item => item.images.downsized.url);
+      this.setState({ gifUrlList: imageUrlList });
+    });
+  }
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+render(<App />, rootElement);
